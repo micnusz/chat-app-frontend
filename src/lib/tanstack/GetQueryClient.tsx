f3032647ed73d6c261600070042c2625de91c57b
Michal <michalnuszkiewiczbusiness@gmail.com>
2025-10-16 22:45:29 +0200

    Created basic react query provider pattern

---
+++ src/lib/tanstack/GetQueryClient.tsx
@@ -0,0 +1,29 @@
+// lib/get-query-client.ts
+import { QueryClient } from "@tanstack/react-query";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 5 * 60 * 1000,
+        gcTime: 2 * 60 * 1000,
+        refetchOnWindowFocus: false,
+        refetchOnMount: true,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+export function getQueryClient() {
+  if (typeof window === "undefined") {
+    return makeQueryClient();
+  }
+
+  if (!browserQueryClient) {
+    browserQueryClient = makeQueryClient();
+  }
+
+  return browserQueryClient;
+}

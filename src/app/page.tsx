9580724e0ade4801767319f45a338c2914e8458f
Michal <michalnuszkiewiczbusiness@gmail.com>
2025-10-17 19:59:12 +0200

    Redirectring user to /chatrooms when logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-// app/page.tsx
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useUserStore } from "@/lib/stores/UserStore";
 import UserForm from "../components/UserForm";
 
-export const dynamic = "force-dynamic";
-
 export default function HomePage() {
+  const router = useRouter();
+  const { user, token } = useUserStore();
+
+  useEffect(() => {
+    if (user && token) {
+      router.replace("/chatrooms");
+    }
+  }, [user, token, router]);
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold">Welcome to Chat App</h1>

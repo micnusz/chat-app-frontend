700bb465d8d29e028a5c8fb724916b556066c835
Michal <michalnuszkiewiczbusiness@gmail.com>
2025-11-06 22:21:25 +0100

    Updated getUserColor function

--- src/lib/helper/getUserColor.ts
+++ src/lib/helper/getUserColor.ts
@@ -7,11 +7,11 @@ export default function getUserColor(username: string) {
   }
 
   const colors = [
-    "oklch(0.72 0.09 250)",
-    "oklch(0.75 0.09 310)",
-    "oklch(0.78 0.09 120)",
-    "oklch(0.68 0.09 40)",
-    "oklch(0.74 0.08 200)",
+    "oklch(0.4 0.0556 250)",
+    "oklch(0.42 0.05 95)",
+    "oklch(0.38 0.045 150)",
+    "oklch(0.39 0.07 30)",
+    "oklch(0.43 0.03 312)",
     "oklch(0.77 0.07 150)",
     "oklch(0.73 0.08 20)",
     "oklch(0.76 0.09 340)",

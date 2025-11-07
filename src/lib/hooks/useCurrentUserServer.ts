47115d682d4106d4d887ed7af7254c814ef46f32
Michal <michalnuszkiewiczbusiness@gmail.com>
2025-11-07 18:58:42 +0100

    Added console logs for debug

--- src/lib/hooks/useCurrentUserServer.ts
+++ src/lib/hooks/useCurrentUserServer.ts
@@ -2,16 +2,27 @@ import { cookies } from "next/headers";
 
 export async function getCurrentUserServer() {
   const cookieStore = cookies();
-  const cookie = (await cookieStore).get("access_token")?.value;
+
+  console.log("SSR Cookies:", (await cookieStore).getAll());
+
+  const cookie = (await cookieStore).get("accessToken")?.value;
+
+  if (!cookie) {
+    console.log("Brak accessToken w SSR");
+  }
 
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/me`, {
     headers: {
-      Cookie: `access_token=${cookie}`,
+      Cookie: `accessToken=${cookie};`,
     },
     credentials: "include",
     cache: "no-store",
   });
 
-  if (!res.ok) return null;
+  if (!res.ok) {
+    console.log("SSR /me failed with status:", res.status);
+    return null;
+  }
+
   return res.json();
 }

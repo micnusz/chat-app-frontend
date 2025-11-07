ba5e5def6495c49216580e7768e6673a061f5d0a
Michal <michalnuszkiewiczbusiness@gmail.com>
2025-11-07 19:23:20 +0100

    Updated for ssr

--- src/lib/hooks/useCurrentUserServer.ts
+++ src/lib/hooks/useCurrentUserServer.ts
@@ -3,26 +3,19 @@ import type { UserResponseDTO } from "@/lib/types";
 
 export async function getCurrentUserServer(): Promise<UserResponseDTO | null> {
   const cookieStore = cookies();
-  console.log("SSR Cookies:", (await cookieStore).getAll());
+
   const cookieHeader = (await cookieStore)
     .getAll()
     .map((c) => `${c.name}=${c.value}`)
     .join("; ");
 
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_PATH || ""}/api/users/me`,
-    {
-      headers: {
-        Cookie: cookieHeader,
-      },
-      credentials: "include",
-      cache: "no-store",
-    }
-  );
+  const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/users/me`, {
+    headers: {
+      Cookie: cookieHeader,
+    },
+    cache: "no-store",
+  });
 
-  if (!res.ok) {
-    console.log("SSR /me failed with status:", res.status);
-    return null;
-  }
+  if (!res.ok) return null;
   return res.json();
 }

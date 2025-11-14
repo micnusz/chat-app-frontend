745af08c21b0d832a558781d425c1b5bb6a07040
Michal <michalnuszkiewiczbusiness@gmail.com>
2025-11-14 19:13:20 +0100

    Updated Footer, updated getUserColor

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { ArrowLeft, FileSymlink, LinkIcon } from "lucide-react";
+import {
+  ArrowLeft,
+  FileSymlink,
+  Github,
+  Linkedin,
+  LinkIcon,
+} from "lucide-react";
 import Link from "next/link";
 
 export default function Footer() {
@@ -8,13 +14,32 @@ export default function Footer() {
     <footer className="px-fluid py-fluid bg-background border-t flex flex-col gap-[4vw]">
       <div className="grid gap-[3rem] w-full md:grid-cols-3">
         <div className="flex flex-col max-w-[45ch]">
-          <h1 className="responsive-h3 text-foreground">CHAT APP</h1>
-          <p className="body-text text-muted-foreground">
-            Free chat application that lets you text with others anytime,
-            anywhere. Stay connected on any device. Fast, simple, and
-            distraction-free messaging.
-          </p>
+          <p className="responsive-h4 text-muted-foreground">Website by:</p>
+          <div className="flex flex-row gap-x-2 ">
+            <span className="responsive-h2 font-bold">Michał Nuszkiewicz</span>
+            <span className="flex flex-row gap-x-2">
+              <a
+                href="https://github.com/micnusz"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex text-foreground transition hover:text-chart-2 body-text"
+                aria-label="GitHub Profile"
+              >
+                <Github className="w-6 h-6" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/micha%C5%82-nuszkiewicz-686241373/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex text-foreground transition hover:text-chart-2 body-text"
+                aria-label="LinkedIn Profile"
+              >
+                <Linkedin className="w-6 h-6" />
+              </a>
+            </span>
+          </div>
         </div>
+
         <div className="flex flex-col gap-[1.2rem]">
           <p className="responsive-h4 text-muted-foreground">NAVIGATION</p>
           <ul className="flex flex-col gap-[0.75rem]">
@@ -42,34 +67,39 @@ export default function Footer() {
           <p className="responsive-h4 text-muted-foreground">INFO</p>
           <ul className="flex flex-col gap-[0.75rem]">
             <li className="flex items-center gap-[0.5rem] text-muted-foreground body-text">
-              Website by:
-              <span className="text-foreground font-semibold body-text">
-                Michał Nuszkiewicz
-              </span>
-            </li>
-
-            <li className="flex items-center gap-[0.5rem] text-muted-foreground body-text">
-              Github:
-              <a
-                href="https://github.com/micnusz"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center gap-[0.35rem] text-foreground transition hover:text-chart-4 body-text"
-              >
-                <LinkIcon className="w-5 h-5" />
-                <span className="hover:underline body-text">Link</span>
-              </a>
-            </li>
-
-            <li className="flex items-center gap-[0.5rem] text-muted-foreground body-text">
               Resume:
               <a
-                href="/Jan-Kowalski-Resume.pdf"
+                href="/resume/CV Michał Nuszkiewicz.pdf"
                 download
                 className="flex items-center gap-[0.35rem] text-foreground transition hover:text-chart-4 body-text"
               >
                 <FileSymlink className="w-5 h-5" />
                 <span className="hover:underline body-text">Download</span>
+              </a>
+            </li>
+            <li className="flex items-center gap-[0.5rem] text-muted-foreground body-text">
+              Github (Back-end):
+              <a
+                href="https://github.com/micnusz/chat-app-backend"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center gap-[0.35rem] text-foreground transition hover:text-chart-4 body-text"
+              >
+                <LinkIcon className="w-5 h-5" />
+                <p className="hover:underline body-text">Link</p>
+              </a>
+            </li>
+
+            <li className="flex items-center gap-[0.5rem] text-muted-foreground body-text">
+              Github (Front-end):
+              <a
+                href="https://github.com/micnusz/chat-app-backend"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center gap-[0.35rem] text-foreground transition hover:text-chart-4 body-text"
+              >
+                <LinkIcon className="w-5 h-5" />
+                <p className="hover:underline body-text">Link</p>
               </a>
             </li>
           </ul>
--- src/lib/helper/getUserColor.ts
+++ src/lib/helper/getUserColor.ts
@@ -7,14 +7,16 @@ export default function getUserColor(username: string) {
   }
 
   const colors = [
-    "oklch(0.4 0.0556 250)",
-    "oklch(0.42 0.1135 351.53)",
-    "oklch(0.38 0.045 150)",
-    "oklch(0.4853 0.0839 210.71)",
-    "oklch(0.43 0.03 312)",
-    "oklch(0.77 0.07 150)",
+    "oklch(0.5235 0.1755 15.87)",
+    "oklch(0.9385 0.1609 83.34)",
+    "oklch(0.6707 0.1925 39.04)",
+    "oklch(0.6406 0.2365 8.07)",
+    "oklch(0.5458 0.2278 295.88)",
+    "oklch(0.6371 0.1753 259.51)",
     "oklch(0.73 0.08 20)",
     "oklch(0.76 0.09 340)",
+    "oklch(0.76 0.1307 217.06)",
+    "oklch(0.76 0.1529 130.24)",
   ];
 
   return colors[Math.abs(hash) % colors.length];
